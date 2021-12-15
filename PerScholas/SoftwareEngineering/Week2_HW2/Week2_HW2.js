3aad31500467b656e1b4d78d9e808c5edce36835
Michael Cartagena <mcartagenaez8@gmail.com>
2021-12-15 09:33:04 -0500

    misplaced Week2_HW2 code re-add here

--- PerScholas/SoftwareEngineering/Week2_HW2/Week2_HW2.js
+++ PerScholas/SoftwareEngineering/Week2_HW2/Week2_HW2.js
@@ -1,49 +1,90 @@
-/* 1. Write a program to add up all the elements of a number array.
- add up elements in a number array */
-const addUp = (arr) => {
-    let sum = 0;
-    // Iterate through all elements 
-    // and add them to sum 
-    for (let i = 0; i < arr.length; i++)
-        sum += arr[i];
-    return sum;
+
+/*  example : var str = "Da.d";
+Question 1 */
+var prompt = require('prompt-sync')();
+var str = prompt('Enter your palindrome: ');
+//var str = "Da.d";
+var strLength = str.length; //capture the length of the original string
+
+// Question 2. Lowercase the string and use the RegExp to remove unwanted characters from it like . or ,
+var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+console.log("The amount of  letters, numbers, and special characters in this string is " + strLength);
+console.log()
+
+var lowRegStr = str.toLowerCase().replace(re, '');
+/* str.toLowerCase() = "Da.d".toLowerCase() = "da.d"
+   str.replace(/[\W_]/g, '') = "Da.d".replace(/[\W_]/g, '') = "dad"
+   var lowRegStr = "dad"; */
+console.log(str + 'converted to lowercase and all non-alphanumeric \ncharacters removed: ' + lowRegStr)
+console.log()
+
+var reverseStr = lowRegStr.split('').reverse().join('');
+/* lowRegStr.split('') = "dad".split('') = ["d", "a", "d"]
+   ["d", "a", "d"].reverse() = ["d", "a", "d"]
+   ["d", "a", "d"].join('') = "dad"
+   So, "dad".split('').reverse().join('') = "dad";
+   And, var reverseStr = "dad"; */
+console.log(str + ' reversed is: ' + reverseStr)
+console.log()
+
+// Check if reverseStr is strictly equals to lowRegStr and return a Boolean using ternary operator
+console.log('Is ' + str + '===' + reverseStr + '? ' + (lowRegStr === reverseStr ? true : false))// "dad" === "dad"? => true
+console.log()
+
+//Question 3
+var str = prompt('Enter your a sentence: ');
+console.log()
+var word = prompt('Enter a word repeated in the sentence: ');
+console.log()
+
+// split the str string by spaces in splitString
+var splitString = str.split(" ");
+
+// search for pattern in splitString
+var count = 0;
+for (let i = 0; i < splitString.length; i++) {
+    // if match found increase count
+    if (word == (splitString[i]))
+        count++;
 }
+console.log("How many times a user entered word occurs in given string " + count + ' times.');
 
-// Driver code
-const arr = [56, 56, 7, 8, 33];
-console.log('\nSum of given array is ' + addUp(arr)+ '\n');
+//Question 4
+var operator = prompt('Enter your calulator input Add,Subtract,Multiply,Divide or Exit: ');
+console.log()
+var numbers = prompt('Enter two numbers to calculate with space in between ');
+console.log()
+var re = /[^0-9]/g;
+console.log()
+var lowRegStr = numbers.toLowerCase().replace(re, ' ');
+console.log(lowRegStr)
+var splitString = lowRegStr.split(' ');
+console.log(splitString)
+var output;
 
-/* 2. Write a program to look for the user input in an array. When it finds the first occurrence, 
-display the value and the index of that value */
-let prompt = require('prompt-sync')();
-const callPrompt = (input = prompt("Given array [56, 56, 7, 8, 33] select which value to find: ")) => input;
-let userinput = Number(callPrompt());
+switch (operator) {
+    case 'Add':
+        output = Number(splitString[0]) + Number(splitString[1]);
+        break;
 
-function getOccurrence(arr, userinput) {
-    //returns a filtered array with only the values entered ex: [56,56] and use .length 
-    //to count its how many times it appears in the array
-    return arr.filter((element) => (element === userinput)).length;
+    case 'Subtract':
+        output = Number(splitString[0]) - Number(splitString[1]);
+        break;
+
+    case 'Multiply':
+        output = Number(splitString[0]) * Number(splitString[1]);
+        break;
+
+    case 'Divide':
+        output = Number(splitString[0]) / Number(splitString[1]);
+        break;
+
+    case 'Exit':
+        break;
+    default:
+        console.log("Invalid Option");
+        return;
 }
-function inputOccursIndex(arr, userinput) {
-    //create new array to store array positions
-    let arrIndexStorage = [];
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i] === userinput) {
-            arrIndexStorage.push(i);
-        }
-    }
-    return arrIndexStorage.toString();
-}
+console.log("The numbers " + splitString[0] + ' ' + operator + ' ' + splitString[1] + " = " + output);
 
-console.log('\n'+ 'The number ' + userinput + ' occurs ' +
-    getOccurrence(arr, userinput) + ' times in the array at index ' + inputOccursIndex(arr, userinput)+ '\n');
-
-/* 3. Write a program to find the longest element in a string array. */
-const longestArr = ['Ali', 'Alex', 'Henry', 'John'];
-const longest = longestArr.reduce(
-    (a, b) => {
-        return a.length > b.length ? a : b;
-    }
-);
-
-console.log('The longest string in the array ' + '[' + longestArr + '] is ' + longest+ '\n')
+var arr=[[1,2],[3,4],[5,6]];
